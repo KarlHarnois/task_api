356b670092edcf692823d2d1d5e3e83539a47a73
KarlHarnois <karl@goDynamo.com>
2018-01-31 15:43:28 -0500

    Clean up task spec
    We inline small before blocks and rewrite the context strings.

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,17 +2,15 @@ require 'rails_helper'
 
 describe Task, type: :model do
   describe '#save' do
-    context 'when name is nil' do
-      before do
-        subject.name = nil
-      end
+    context 'when task name is nil' do
+      before { subject.name = nil }
 
       it 'does not save the task' do
         expect(subject.save).to be false
       end
     end
 
-    context 'when named' do
+    context 'when task is named' do
       before { subject.name = 'someName' }
 
       it 'saves the task' do
