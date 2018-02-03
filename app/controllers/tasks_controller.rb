80270ea58cc4fe31c09ccdb495a20229005e6c7e
Karl Rivest Harnois <karlrivestharnois@gmail.com>
2018-02-03 18:26:53 -0500

    Refactor/Early Returns Should Render Errors (#21)
    * Extract error rendering into a private method
    
    We remove some code repetition by moving the error rendering logic into
    a method. We might create some helper object in the future to hold the
    error logic outside of the controller.
    
    * Return early on error in TaskController
    
    It's unusual to return early on success when using guard statements so
    we do the opposite and return only when an error happens.
    
    * Move errors method under render_422
    
    Following the newspaper metaphor, we move the method dealing with finer
    details at the bottom under the caller `render_422`.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,8 @@
 class TasksController < ApplicationController
   def create
     @task = Task.create(task_params)
-    render json: @task, status: 201 and return if @task.save
-    render json: errors, status: 422
+    render_422 and return unless @task.save
+    render json: @task, status: 201
   end
 
   def index
@@ -11,8 +11,8 @@ class TasksController < ApplicationController
 
   def update
     @task = Task.find(params[:id])
-    render json: @task and return if update_task!
-    render json: errors, status: 422
+    render_422 and return unless update_task!
+    render json: @task
   end
 
   private
@@ -21,11 +21,15 @@ class TasksController < ApplicationController
     params.permit(:name)
   end
 
-  def errors
-    { errors: @task.errors.full_messages }
-  end
-
   def update_task!
     @task.update_attributes(task_params)
+  end
+
+  def render_422
+    render json: errors, status: 422
+  end
+
+  def errors
+    { errors: @task.errors.full_messages }
   end
 end
