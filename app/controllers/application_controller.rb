3de0b758bf9d7e1645a6f48df2fe2048368416ed
Karl Rivest Harnois <karlrivestharnois@gmail.com>
2018-02-06 09:59:25 -0500

    Refactoring/Errors (#28)
    * Remove custom 404 error
    
    We are now relying on the framework error instead of providing a very
    similar custom implementation.
    
    * Conform 422 to rails error schema
    
    We want all of our errors to have the same structure so we use the rails
    errors schema for our 422. We also add all of the record's errors to the
    error body instead of only the first one, to provide more information on
    what went wrong.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,7 @@ class ApplicationController < ActionController::API
   http_basic_authenticate_with name: Rails.application.secrets.auth_username,
                                password: Rails.application.secrets.auth_password
 
-  rescue_from ActiveRecord::RecordNotFound, with: :render_404
-
-  def render_404(error)
-    render json: { error: { message: error.message } }, status: :not_found
-  end
-
   def render_422(record)
-    render json: { error: { message: record.errors.full_messages.first } }, status: 422
+    render json: { error: record.errors.full_messages.join(', ') }, status: 422
   end
 end
